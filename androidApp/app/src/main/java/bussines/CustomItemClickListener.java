fa8e71f64c7d81ff36e637c9d28b4f375bd0601c
ali zohair <alizohair89@outlook.com>
2017-10-22 17:11:03 +0500

    appointmentList

---
+++ androidApp/app/src/main/java/bussines/CustomItemClickListener.java
@@ -0,0 +1,12 @@
+package bussines;
+
+import android.view.View;
+
+/**
+ * Created by Hp on 10/22/2017.
+ */
+
+public interface CustomItemClickListener {
+
+    public void onItemClick(View v, int position);
+}
